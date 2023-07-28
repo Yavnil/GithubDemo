099d058136abcca531bab051ce05d61597a25306
Yavnil <ayavnia@gmail.com>
2023-07-28 10:27:52 -0700

    Initial Commit

---
+++ GithubDemo/GithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoApp.swift
+//  GithubDemo
+//
+//  Created by Al Yavnilovitch on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
